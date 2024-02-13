28c32b9ff281a1ab8944efb26ca12acbff8f3d5b
BELLORI <bellori729@gmail.com>
2024-02-13 22:03:48 +0900

    hooks 확장 및 규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,12 +8,16 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: ["react", "react-hooks"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
 };
