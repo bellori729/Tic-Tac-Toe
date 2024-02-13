60297054cf325067868306aa05409147c7bd7bdd
BELLORI <bellori729@gmail.com>
2024-02-13 22:53:58 +0900

    Board.css 삭제 및 TailwindCss로 적용

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -54,23 +54,23 @@ const Board = ({ xIsNext, squares, onPlay }) => {
   }
 
   return (
-    <div className="playBoard">
-      <div className="board-row">
+    <div className="playBoard flex flex-col items-center">
+      <div className="board-row after:content-[''] after:table">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div className="board-row">
+      <div className="board-row after:content-[''] after:table">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="board-row">
+      <div className="board-row after:content-[''] after:table">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-      <div className="status">
+      <div className="status mt-2.5">
         <p>{status}</p>
       </div>
     </div>
