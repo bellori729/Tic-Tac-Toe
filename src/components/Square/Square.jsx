fdb4652952624f11e795ef22ec3e6032c04ec516
BELLORI <bellori729@gmail.com>
2024-02-13 23:12:07 +0900

    Square.css 삭제 및 tailwind CSS로 변환

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,9 +1,10 @@
-import "./Square.css";
-
 const Square = ({ value, onSquareClick }) => {
   return (
-    <button className="square" onClick={onSquareClick}>
-      <p>{value}</p>
+    <button
+      className="square flex justify-center items-center border-black border bg-white float-left text-black -mr-1 -mt-1 p-0 h-24 w-24"
+      onClick={onSquareClick}
+    >
+      <p className="text-center text-6xl font-bold">{value}</p>
     </button>
   );
 };
