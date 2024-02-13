0d8b18cf104f5dd156648e1fef4339b94f1474d3
BELLORI <bellori729@gmail.com>
2024-02-13 22:05:52 +0900

    eslint jsx a11y 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,15 +9,17 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
   ],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "react-hooks"],
+  plugins: ["react", "react-hooks", "jsx-a11y"],
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/rule-name": 2,
   },
 };
